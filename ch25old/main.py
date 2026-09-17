34f423597453ede8e0449a10a4e1e55d72ca714d
Akshay <akshaythigale7101@gmail.com>
2026-09-18 00:46:19 +0530

    method is umcomment

--- ch25old/main.py
+++ ch25old/main.py
@@ -21,10 +21,10 @@ class Productout(BaseModel):
 #     return {"name": "akshay", "age":29,"email":"akshay@gmail.com"}
 
 #with respomce model paramter
-# @app.get("/info/",response_model=Proudct)
-# def read_root():
-#     return {"name": "akshay", "age":29,"email":"akshay@gmail.com"}
-#     #return {"Hello": "World"} #--get error cause reponce model is used
+@app.get("/info/",response_model=Proudct)
+def read_root():
+    return {"name": "akshay", "age":29,"email":"akshay@gmail.com"}
+    #return {"Hello": "World"} #--get error cause reponce model is used
 
 
 @app.post("/proudcts/",response_model=Proudct)
@@ -42,17 +42,17 @@ class Userout(UserIn):
     passward:str
 
 
-@app.post("/info/",response_model=UserIn)
-def read_root(user:Userout):
-    return user
-
-
 # @app.post("/info/",response_model=UserIn)
-# def read_root(user:Userout)->Any:
+# def read_root19(user:Userout):
 #     return user
+
 #
-# @app.post("/info/",response_model=None)#siable
-# def read_root(user:Userout)->Any:
+# @app.post("/info/",response_model=UserIn)
+# def read_data(user:Userout)->Any:
 #     return user
+
+@app.post("/info/",response_model=None)#siable
+def read_info(user:Userout)->Any:
+    return user
 
 
