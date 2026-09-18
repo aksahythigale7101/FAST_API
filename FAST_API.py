1607a30ba07f59155dfa1600353f6d3bd97b3a6e
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-09-18 11:39:37 +0530

    comment they calling code

--- FAST_API.py
+++ FAST_API.py
@@ -1,8 +1,8 @@
-import asyncio
+# import asyncio
 
-print("This is fastapi project  !!")
+# print("This is fastapi project  !!")
 
-import ch31.asyncversion.main
+# import ch31.asyncversion.main
 
-asyncio.run(ch31.asyncversion.main.mainfile())
+# asyncio.run(ch31.asyncversion.main.mainfile())
 
