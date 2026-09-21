09975ba1ba65bffe81b02c73a8136efc1637bded
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-09-21 18:12:48 +0530

    call middleware method

--- fastapi_async_project/app/main.py
+++ fastapi_async_project/app/main.py
@@ -1,12 +1,23 @@
-
+import time
+import uuid
 from fastapi import FastAPI
 
 from app.routers.product import router as product_router
-
-
+from app.middleware.custom_middleware import *
+from app.middleware.cors import setup_cors
 app = FastAPI(title="Product Management API", version="1.0.0")
 
+# Middleware
+app.middleware("http")(request_id_middleware)
+app.middleware("http")(logging_middleware)
+setup_cors(app)
 
+
+
+
+
+
+# Router
 app.include_router(product_router)
 
 
