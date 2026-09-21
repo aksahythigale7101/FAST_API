b6bdd93519debaa221b8d84e5c5dd6af42bac930
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-09-21 18:16:08 +0530

    add in middkeware folder

---
+++ Middleware/main.py
@@ -0,0 +1,48 @@
+﻿from fastapi import FastAPI, Request
+from middle.mdlware import *#middleware_one,middleware_two
+
+
+app = FastAPI()
+
+
+
+
+# app.middleware("http")(middleware_one)
+# app.middleware("http")(middleware_two)
+#app.middleware("http")(path_middleware)
+#app.add_middleware(MyMiddleware)
+app.add_middleware(LoggingMiddleware)
+
+@app.get("/")
+async def home():
+    print("Inside endpoint")
+    return {"message": "Hello"}
+
+
+
+
+#Simple Path-Specific Middleware
+@app.get("/admin")
+async def admin():
+    return {"message": "Admin page"}
+
+
+@app.get("/products")
+async def products():
+    return {"message": "Products"}
+
+
+# @app.middleware("http")#"हा function HTTP middleware म्हणून वापर."
+# async def my_middleware(request: Request, call_next):#Client ने पाठवलेली request.
+
+#     print("Before endpoint")
+
+#     response = await call_next(request)#Request ला पुढच्या processing कडे पाठव.जर call_next() call केला नाही तर request पुढे endpoint पर्यंत जाणार नाही.
+
+#     print("After endpoint")
+
+#     return response
+
+
+
+
---
+++ Middleware/middle/mdlware.py
@@ -0,0 +1,73 @@
+﻿from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.types import ASGIApp, Scope, Receive, Send
+
+
+class MyMiddleware(
+    BaseHTTPMiddleware
+):  # BaseHTTPMiddleware हा Starlette कडून मिळणारा base class आहे.
+
+    async def dispatch(self, request, call_next):
+
+        print("Before request")
+
+        response = await call_next(request)
+
+        print("After request")
+
+        return response
+
+
+async def middleware_one(request, call_next):
+
+    print("M1 Before")
+
+    response = await call_next(request)
+
+    print("M1 After")
+
+    return response
+
+
+async def middleware_two(request, call_next):
+
+    print("M2 Before")
+
+    response = await call_next(request)
+
+    print("M2 After")
+
+    return response
+
+
+# Simple Path-Specific Middleware
+async def path_middleware(request, call_next):
+
+    # if request.url.path == "/admin":
+    #     print("Admin middleware executed")
+
+    if request.url.path in ["/admin", "/users"]:
+        print("Special middleware logic")
+
+    response = await call_next(request)
+
+    return response
+
+#Async Server Gateway Interface
+class LoggingMiddleware:
+
+    def __init__(self, app: ASGIApp):
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send):
+
+        if scope["type"] != "http":
+            await self.app(scope, receive, send)
+            return
+
+        print("Before Request")
+        print("Method:", scope["method"])
+        print("Path:", scope["path"])
+
+        await self.app(scope, receive, send)
+
+        print("After Response")
