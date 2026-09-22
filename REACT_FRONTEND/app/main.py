ba3e6710880697cd3c5979edfcdc91bf1539bd82
Akshay <akshaythigale7101@gmail.com>
2026-09-23 00:49:11 +0530

    FRONDEND Name Change to FRontendJS

---
+++ REACT_FRONTEND/app/main.py
@@ -0,0 +1,34 @@
+import time
+import uuid
+from fastapi import FastAPI
+
+from app.routers.product import router as product_router
+from app.middleware.custom_middleware import *
+from app.middleware.cors import setup_cors
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="Product Management API", version="1.0.0")
+
+# Middleware
+app.add_middleware(
+    CORSMiddleware,
+
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
+
+    allow_credentials=True,
+
+    allow_methods=["*"],
+
+    allow_headers=["*"],
+)
+
+# Router
+app.include_router(product_router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Product API is running"}
