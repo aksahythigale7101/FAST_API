eb7db89c673bb7cb54e4d0445f799d559335c702
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-09-24 13:45:54 +0530

    api key create

--- auth_project/app/routers/auth.py
+++ auth_project/app/routers/auth.py
@@ -4,10 +4,7 @@ from sqlmodel import Session
 from app.db.database import get_Session
 from app.schemas.user import UserCreate, UserResponse
 
-from app.services.auth_service import (
-    login_user,
-    register_user
-)
+from app.services.auth_service import login_user, register_user
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app.schemas.token import TokenResponce
@@ -25,10 +22,26 @@ def register(user_data: UserCreate, session: Annotated[Session, Depends(get_Sess
 
 
 @router.post("/login", response_model=TokenResponce, status_code=status.HTTP_200_OK)
-def login(from_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-          session: Annotated[Session, Depends(get_Session)]):
+def login(
+    from_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: Annotated[Session, Depends(get_Session)],
+):
     try:
         access_token = login_user(session, from_data.username, from_data.password)
         return {"access_token": access_token, "token_type": "bearer"}
     except ValueError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+
+import secrets
+
+current_user = {
+    "id": 10,
+    "name": "Akshay"
+}
+@router.post("/api-keys",tags=["temp"])
+async def create_api_key(current_user:dict):
+
+    api_key = secrets.token_urlsafe(32)
+
+    return {"user_id": current_user["id"], "api_key": api_key}
